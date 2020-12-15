603bf8b7f5d9dca6416b3fef943629a8749e3482
duooi <duooiChen@163.com>
2020-12-15 22:36:05 +0800

    20201215 2236

---
+++ 第四阶段：H5 + C3/Day 01-02/js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+    /**
+     * 初始化fullpage组件
+     */
+    $("#fullpage").fullpage({
+        // 设置各屏背景色
+        sectionsColor:["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+        // 设置fullpage导航
+        navigation:true,
+        // 页面初始化后执行
+        afterRender:function(){
+            // 往下点击按钮
+            $(".btn_more").click(function(){
+                $.fn.fullpage.moveSectionDown();
+            });
+        },
+        // 当某一屏加载后
+        afterLoad:function(){
+            $(this).addClass("now");
+        }
+                
+    });
+});
