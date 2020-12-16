8b0e4e28b71d1c0842e13ea7965b4de945e684e8
duooiChen <duooiChen@163.com>
2020-12-16 22:01:01 +0800

    20201216 2201

--- 第四阶段：H5 + C3/Day 01-02/js/index.js
+++ 第四阶段：H5 + C3/Day 01-02/js/index.js
@@ -15,14 +15,16 @@ $(function(){
             });
         },
         // 当某一屏加载后
-        afterLoad:function(){
+        afterLoad:function(orgin,index){
             $(this).addClass("now");
+            // 处理第四屏的购物车搬运物件
+            if(index == 4){
+                $(".section03 .img_sofa").hide();
+            }
         },
         // 当某一屏离开后
         onLeave:function(index,nextIndex){
-            if(index == 2 && nextIndex == 3){
-                $(this).addClass('leave');
-            }
+            $(this).addClass("leave");
         }
                 
     });
