1294fd4db9d5694d12996cbbe0888c43072f6318
duooiChen <duooiChen@163.com>
2020-12-31 08:44:30 +0800

    20201231 0844

---
+++ 第四阶段：H5 + C3/Day 04/02-history/案例：捉鬼敢死队/api/data.php
@@ -0,0 +1,26 @@
+<?php
+  
+  header("Content-Type","application/json");
+  $html = "";
+  $page = "";
+
+  // 如果接收到的page参数为index或者没接收到page
+  if($_GET['page'] == "index" || !$_GET["page"]){
+    $page = "index";
+  }else if($_GET["page"] == "bill"){
+    $page = "bill";
+    $html = "BILL";
+  }else if($_GET["page"] == "dan"){
+    $page = "dan";
+    $html = "DAN";
+  }else if($_GET["page"] == "egon"){
+    $page = "egon";
+    $html = "EGON";
+  }else if($_GET["page"] == "winston"){
+    $page = "winston";
+    $html = "WINSTON";
+  }
+
+$obj = { page : $page , html : $html };
+
+echo $obj;
